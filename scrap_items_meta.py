24328e0fb73a077ec162ad842bb36c17d101a607
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2020-04-02 10:56:55 +0300

    test commit 2

--- scrap_items_meta.py
+++ scrap_items_meta.py
@@ -1,5 +1,16 @@
+import lxml as lxml
+import pandas as pd
+from urllib import urlopen
+from bs4 import BeautifulSoup
 # get url of the page containing product items, scrap items metadata and save to DB
 class ScrapMetadata:
 
     def __init__(self, url):
         self.url = url
+        
+    def scrap_items(self):
+        #get html of the page
+        html = urlopen(self.url)
+        #create BeautifulSoup object
+        soup = BeautifulSoup(html, 'lxml')
+        
